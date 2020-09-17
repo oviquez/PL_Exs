2dfe4b283ed36114401d97d06dfb08ed7feaef35
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-09-16 19:29:43 -0600

    Ejercicio #1 de la practica #1

---
+++ G50/Eduardo Binns/Practica #1/Ejercicio #1/cmake-build-debug/CMakeFiles/Ejercicio__1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio__1.dir/main.c.obj"
+  "Ejercicio__1.exe"
+  "Ejercicio__1.exe.manifest"
+  "Ejercicio__1.lib"
+  "Ejercicio__1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ejercicio__1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
